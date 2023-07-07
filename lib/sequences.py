3b04454e7af54aa971c65660a35c6fd3d5ecdc3c
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-07-07 17:09:39 -0500

    complete sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0: # return empty list for 0 
+        print([])
+    elif length <= 1:
+        print([0])
+    elif length <= 2:
+        print([0, 1])
+    
+    else:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+
+'''
+    while len(fib) < length:
+        nextNum = fib[-1] + fib[-2]
+        fib.append(nextNum)
+        print(fib)
+print_fibonacci(10)
+'''
